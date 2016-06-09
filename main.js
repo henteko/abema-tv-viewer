0d2643de54f6c601167767f8895ced3e234e8d2d
henteko <henteko07@gmail.com>
2016-06-09 19:23:15 +0900

    menuを追加した

--- main.js
+++ main.js
@@ -1,21 +1,59 @@
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-
+const {app, BrowserWindow, Menu} = require('electron');
+const rootURL = 'https://abema.tv';
 let mainWindow;
 
-function createWindow () {
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+const template = [
+  {
+    label: 'AppName',
+    submenu: [
+      {
+        label: '番組表',
+        accelerator: 'Cmd+Ctrl+T',
+        click: function() { createWindow(rootURL + '/timetable'); }
+      },
+      {
+        label: 'Quit',
+        accelerator: 'Cmd+Q',
+        click: function() { app.quit(); }
+      }
+    ]
+  },
+  {
+    label: 'Window',
+    role: 'window',
+    submenu: [
+      {
+        label: 'New Window',
+        accelerator: 'CmdOrCtrl+T',
+        click: function() { createWindow(rootURL); }
+      },
+      {
+        label: 'Minimize',
+        accelerator: 'CmdOrCtrl+M',
+        role: 'minimize'
+      },
+      {
+        label: 'Close',
+        accelerator: 'CmdOrCtrl+W',
+        role: 'close'
+      }
+    ]
+  }
+];
 
-  mainWindow.loadURL('https://abema.tv');
-  mainWindow.webContents.openDevTools();
+function createWindow (url) {
+  mainWindow = new BrowserWindow({width: 1050, height: 600});
+  mainWindow.loadURL(url);
 
   mainWindow.on('closed', function () {
     mainWindow = null
   });
+
+  let menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 }
 
-app.on('ready', createWindow);
+app.on('ready', function () { createWindow(rootURL) });
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
